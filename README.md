bd2b816e110ecb5fb94ed60ac8c965853e83cb43
Ian Preston <ianopolous@gmail.com>
2016-10-09 17:32:56 +0100

    Initial extraction

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # java-multihash
 A Java implementation of Multihash
+
+## Usage
+Multihash m = Multihash.fromBase58("QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy");
+
+## Compilation
+To compile just run ant.
---
+++ src/test/java/org/ipfs/api/MultihashTests.java
@@ -0,0 +1,20 @@
+package org.ipfs.api;
+
+import org.junit.*;
+
+import java.util.*;
+
+public class MultihashTests {
+
+    @Test
+    public void base58Test() {
+        List<String> examples = Arrays.asList("QmPZ9gcCEpqKTo6aq61g2nXGUhM4iCL3ewB6LDXZCtioEB",
+                "QmatmE9msSfkKxoffpHwNLNKgwZG8eT9Bud6YoPab52vpy");
+        for (String example: examples) {
+            byte[] output = Base58.decode(example);
+            String encoded = Base58.encode(output);
+            if (!encoded.equals(encoded))
+                throw new IllegalStateException("Incorrect base58! " + example + " => " + encoded);
+        }
+    }
+}
