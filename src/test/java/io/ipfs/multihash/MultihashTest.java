c3f2179e7b1bfefc1ca5709dfd17188dfdfc1e4f
Chuck LeDuc Díaz <celeduc@gmail.com>
2017-02-24 23:13:49 +0100

    fix hash function lookups

--- src/test/java/io/ipfs/multihash/MultihashTest.java
+++ src/test/java/io/ipfs/multihash/MultihashTest.java
@@ -23,19 +23,19 @@ public class MultihashTest {
     public void multihashTest() {
         Object[][] examples = new Object[][]{
             {Multihash.Type.sha1, "SHA-1", "5drNu81uhrFLRiS4bxWgAkpydaLUPW", "hello world"},
-            {Multihash.Type.sha2_256, "SHA2-256", "QmaozNR7DZHQK1ZcU9p7QdrshMvXqWK6gpu5rmrkPdT3L4", "hello world"},
-            {Multihash.Type.sha2_512, "SHA2-512", "8Vtkv2tdQ43bNGdWN9vNx9GVS9wrbXHk4ZW8kmucPmaYJwwedXir52kti9wJhcik4HehyqgLrQ1hBuirviLhxgRBNv", "hello world"},
-            {Multihash.Type.sha3_512, "SHA3-512", "8tWhXW5oUwtPd9d3FnjuLP1NozN3vc45rmsoWEEfrZL1L6gi9dqi1YkZu5iHb2HJ8WbZaaKAyNWWRAa8yaxMkGKJmX", "hello world"},
+            {Multihash.Type.sha2_256, "SHA-256", "QmaozNR7DZHQK1ZcU9p7QdrshMvXqWK6gpu5rmrkPdT3L4", "hello world"},
+            {Multihash.Type.sha2_512, "SHA-512", "8Vtkv2tdQ43bNGdWN9vNx9GVS9wrbXHk4ZW8kmucPmaYJwwedXir52kti9wJhcik4HehyqgLrQ1hBuirviLhxgRBNv", "hello world"}
+// SHA3 not yet implemented in standard Java library. https://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#MessageDigest
+//            {Multihash.Type.sha3_512, "SHA3-512", "8tWhXW5oUwtPd9d3FnjuLP1NozN3vc45rmsoWEEfrZL1L6gi9dqi1YkZu5iHb2HJ8WbZaaKAyNWWRAa8yaxMkGKJmX", "hello world"}
         };
 
         for(Object[] ex: examples) {
             Multihash m = Multihash.fromBase58((String)ex[2]);
             try {
-                // Make sure the hashes agree
-                MessageDigest md = MessageDigest.getInstance((String) ex[3]);
+                MessageDigest md = MessageDigest.getInstance((String) ex[1]);
+                assert(md != null);
                 md.update(((String) ex[3]).getBytes("UTF-8"));
                 byte[] digest = md.digest();
-                assert(Arrays.equals(m.toBytes(), digest));
                 // Test constructor
                 Multihash m2 = new Multihash((Multihash.Type)ex[0], digest);
                 // Test comparison
@@ -45,6 +45,9 @@ public class MultihashTest {
                 assert(m.toBase58().equals((String)ex[2]));
             }
             catch (Exception e){
+                // Usually because a hash function not supported
+                System.out.println(e.getMessage());
+                assert(false);
             }
         }
     }
